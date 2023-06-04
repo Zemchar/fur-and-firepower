c7ea84522c45b4c7d348bc18cd9202ed2399aa89
Oz <omxyoussef@gmail.com>
2023-06-03 21:53:36 -0400

    more grid spawner work, my brain is melting
    actually made some progress, random generation is working and detection of walls/roads is working, just gotta combine the two to make the generation work correctly

--- Assets/Scripts/GridSpawner.cs
+++ Assets/Scripts/GridSpawner.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GridSpawner : MonoBehaviour
 {
+    [SerializeField] private LayerMask layerMask;
     [SerializeField] private GameObject[] roadPiece;
     [SerializeField] private int gridSize = 7; //size of the grid
     private int tileWidth = 60; //size of each grid tile
+    private GameObject[,] grid;
 
     private Dictionary<string, GameObject> pieces = new Dictionary<string, GameObject>(); //keeps track of all 17 version of pieces
 
@@ -14,18 +17,29 @@ public class GridSpawner : MonoBehaviour
     {
         FillDictionary();
 
-        GameObject temp;
-
         gridSize += 2; //just to add a border around the grid
+        grid = new GameObject[gridSize, gridSize];
+
+        GridGeneration();
+
+
+    }
+
+    private void GridGeneration()
+    {
+        GameObject temp;
+        int x;
+        int y;
+
         for (var row = 0; row < gridSize; row++)
         {
             for (var col = 0; col < gridSize; col++)
             {
                 //converts row and col numbers into usable x and y coordinates
-                int x = tileWidth * (col - gridSize / 2);
-                int y = tileWidth * (row - gridSize / 2);
+                x = tileWidth * (col - gridSize / 2);
+                y = tileWidth * (row - gridSize / 2);
 
-                if (row == 0 || col == 0 || row == gridSize-1 || col == gridSize-1) //coordinates of the border of the grid
+                if (row == 0 || col == 0 || row == gridSize - 1 || col == gridSize - 1) //coordinates of the border of the grid
                 {
                     if (row == gridSize / 2 && col == 0)
                         temp = Instantiate(pieces["deadend-right"], new Vector3(x, 0, y), pieces["deadend-right"].transform.rotation); //left-most piece
@@ -38,111 +52,331 @@ public class GridSpawner : MonoBehaviour
                     else
                         temp = Instantiate(roadPiece[1], new Vector3(x, 0, y), roadPiece[1].transform.rotation);
 
-                    temp.transform.SetParent(this.transform);
+
+                    temp.transform.SetParent(this.transform.Find("Border").transform);
+                    grid[row, col] = temp;
                 }
-                else if(row == gridSize / 2 || col == gridSize / 2) //makes a cross in the grid, filling it with 4-way pieces
+                else if (row == gridSize / 2 || col == gridSize / 2) //makes a cross in the grid, filling it with 4-way pieces
                 {
                     temp = Instantiate(pieces["cross"], new Vector3(x, 0, y), pieces["cross"].transform.rotation);
                     temp.transform.SetParent(this.transform);
+                    grid[row, col] = temp;
                 }
-                else //delete this entire chunk later, just filler until everything works properly
+                else //randomly fills grid, delete later
                 {
-                    temp = Instantiate(pieces["empty"], new Vector3(x, 0, y), pieces["empty"].transform.rotation);
+                    var randomKey = pieces.Keys.ElementAt((int)Random.Range(2, pieces.Keys.Count));
+
+                    temp = Instantiate(pieces[randomKey], new Vector3(x, 0, y), pieces[randomKey].transform.rotation);
                     temp.transform.SetParent(this.transform);
+                    grid[row, col] = temp;
+
                 }
             }
         }
 
 
+        for (var row = 1; row < gridSize - 1; row++)
+        {
+            for (var col = 1; col < gridSize - 1; col++)
+            {
+                x = tileWidth * (col - gridSize / 2);
+                y = tileWidth * (row - gridSize / 2);
 
+                foreach (BoxCollider box in grid[row, col].transform.Find("Colliders").GetComponentsInChildren<BoxCollider>())
+                    box.enabled = false;
+                Debug.Log(PieceChecker(x, y)); 
+                foreach (BoxCollider box in grid[row, col].transform.Find("Colliders").GetComponentsInChildren<BoxCollider>())
+                    box.enabled = true;
+            }
+        }
+
+
+
+
+        /*
+        //bottom left quadrant
+        int tempRow = Random.Range(1, gridSize / 2);
+        int tempCol = Random.Range(1, gridSize / 2);
+        //converts row and col numbers into usable x and y coordinates
+        x = tileWidth * (tempRow - gridSize / 2);
+        y = tileWidth * (tempCol - gridSize / 2);
+
+        if (grid[tempRow, tempCol] == null)
+        {
+            temp = Instantiate(pieces["cross"], new Vector3(x, 0, y), pieces["cross"].transform.rotation);
+            temp.transform.SetParent(this.transform);
+            grid[tempRow, tempCol] = temp;
+        }
+        else
+            Debug.Log("aaa");
+
+        //bottom right quadrant
+        tempRow = Random.Range(gridSize / 2 + 1, gridSize - 1);
+        tempCol = Random.Range(1, gridSize / 2);
+        //converts row and col numbers into usable x and y coordinates
+        x = tileWidth * (tempRow - gridSize / 2);
+        y = tileWidth * (tempCol - gridSize / 2);
+
+        if (grid[tempRow, tempCol] == null)
+        {
+            temp = Instantiate(pieces["cross"], new Vector3(x, 0, y), pieces["cross"].transform.rotation);
+            temp.transform.SetParent(this.transform);
+            grid[tempRow, tempCol] = temp;
+        }
+        else
+            Debug.Log("aaa");
+
+        //top left quadrant
+        tempRow = Random.Range(1, gridSize / 2);
+        tempCol = Random.Range(gridSize / 2 + 1, gridSize - 1);
+        //converts row and col numbers into usable x and y coordinates
+        x = tileWidth * (tempRow - gridSize / 2);
+        y = tileWidth * (tempCol - gridSize / 2);
+
+        if (grid[tempRow, tempCol] == null)
+        {
+            temp = Instantiate(pieces["cross"], new Vector3(x, 0, y), pieces["cross"].transform.rotation);
+            temp.transform.SetParent(this.transform);
+            grid[tempRow, tempCol] = temp;
+        }
+        else
+            Debug.Log("aaa");
+
+        //top right quadrant
+        tempRow = Random.Range(gridSize / 2 + 1, gridSize - 1);
+        tempCol = Random.Range(gridSize / 2 + 1, gridSize - 1);
+        //converts row and col numbers into usable x and y coordinates
+        x = tileWidth * (tempRow - gridSize / 2);
+        y = tileWidth * (tempCol - gridSize / 2);
+
+        if (grid[tempRow, tempCol] == null)
+        {
+            temp = Instantiate(pieces["cross"], new Vector3(x, 0, y), pieces["cross"].transform.rotation);
+            temp.transform.SetParent(this.transform);
+            grid[tempRow, tempCol] = temp;
+        }
+        else
+            Debug.Log("aaa");
+        */
     }
 
+    private string PieceChecker(int x, int y)
+    {
+        int height = 15;
+        List<string> possiblePieces = new List<string> { "deadend-down", "deadend-left", "deadend-up", "deadend-right", "straight-vertical", "straight-horizontal", "L-down", "L-left", "L-up", "L-right", "T-down", "T-left", "T-up", "T-right", "cross" };
+
+        RaycastHit hit;
+        if (Physics.Raycast(new Vector3(x, height, y), transform.TransformDirection(Vector3.back), out hit, 40, layerMask))
+        {
+            Debug.DrawRay(new Vector3(x, height, y), transform.TransformDirection(Vector3.back) * hit.distance, Color.green, Mathf.Infinity);
+            if (hit.transform.tag == "Wall")
+            {
+                //Debug.Log("Hitting a Back Wall");
+                possiblePieces.Remove("deadend-down");
+                possiblePieces.Remove("straight-vertical");
+                possiblePieces.Remove("L-down");
+                possiblePieces.Remove("L-left");
+                possiblePieces.Remove("T-down");
+                possiblePieces.Remove("T-left");
+                possiblePieces.Remove("T-right");
+                possiblePieces.Remove("cross");
+            }
+            if (hit.transform.tag == "Road")
+            {
+                //Debug.Log("Hitting a Back Road");
+                possiblePieces.Remove("deadend-left");
+                possiblePieces.Remove("deadend-up");
+                possiblePieces.Remove("deadend-right");
+                possiblePieces.Remove("straight-horizontal");
+                possiblePieces.Remove("L-up");
+                possiblePieces.Remove("L-right");
+                possiblePieces.Remove("T-up");
+            }
+        }
+        else
+            Debug.Log("Hitting nothing back");
+
+        if (Physics.Raycast(new Vector3(x, height, y), transform.TransformDirection(Vector3.left), out hit, 40, layerMask))
+        {
+            Debug.DrawRay(new Vector3(x, height, y), transform.TransformDirection(Vector3.left) * hit.distance, Color.red, Mathf.Infinity);
+            if (hit.transform.tag == "Wall")
+            {
+                //Debug.Log("Hitting a Left Wall");
+                possiblePieces.Remove("deadend-left");
+                possiblePieces.Remove("straight-horizontal");
+                possiblePieces.Remove("L-left");
+                possiblePieces.Remove("L-up");
+                possiblePieces.Remove("T-down");
+                possiblePieces.Remove("T-left");
+                possiblePieces.Remove("T-up");
+                possiblePieces.Remove("cross");
+            }
+            if (hit.transform.tag == "Road")
+            {
+                //Debug.Log("Hitting a Left Road");
+                possiblePieces.Remove("deadend-down");
+                possiblePieces.Remove("deadend-up");
+                possiblePieces.Remove("deadend-right");
+                possiblePieces.Remove("straight-vertical");
+                possiblePieces.Remove("L-down");
+                possiblePieces.Remove("L-right");
+                possiblePieces.Remove("T-right");
+            }  
+        }
+        else
+            Debug.Log("Hitting nothing left");
+
+        if (Physics.Raycast(new Vector3(x, height, y), transform.TransformDirection(Vector3.right), out hit, 40, layerMask))
+        {
+            Debug.DrawRay(new Vector3(x, height, y), transform.TransformDirection(Vector3.right) * hit.distance, Color.blue, Mathf.Infinity);
+            if (hit.transform.tag == "Wall")
+            {
+                //Debug.Log("Hitting a Right Wall");
+                possiblePieces.Remove("deadend-right");
+                possiblePieces.Remove("straight-horizontal");
+                possiblePieces.Remove("L-down");
+                possiblePieces.Remove("L-right");
+                possiblePieces.Remove("T-down");
+                possiblePieces.Remove("T-up");
+                possiblePieces.Remove("T-right");
+                possiblePieces.Remove("cross");
+            }  
+            if (hit.transform.tag == "Road")
+            {
+                //Debug.Log("Hitting a Right Road");
+                possiblePieces.Remove("deadend-down");
+                possiblePieces.Remove("deadend-left");
+                possiblePieces.Remove("deadend-up");
+                possiblePieces.Remove("straight-vertical");
+                possiblePieces.Remove("L-left");
+                possiblePieces.Remove("L-up");
+                possiblePieces.Remove("T-left");
+            }  
+        }
+        else
+            Debug.Log("Hitting nothing right");
+
+        if (Physics.Raycast(new Vector3(x, height, y), transform.TransformDirection(Vector3.forward), out hit, 40, layerMask))
+        {
+            Debug.DrawRay(new Vector3(x, height, y), transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow, Mathf.Infinity);
+            if (hit.transform.tag == "Wall")
+            {
+                //Debug.Log("Hitting a Forward Wall");
+                possiblePieces.Remove("deadend-up");
+                possiblePieces.Remove("straight-vertical");
+                possiblePieces.Remove("L-up");
+                possiblePieces.Remove("L-right");
+                possiblePieces.Remove("T-left");
+                possiblePieces.Remove("T-up");
+                possiblePieces.Remove("T-right");
+                possiblePieces.Remove("cross");
+            }
+            if (hit.transform.tag == "Road")
+            {
+                //Debug.Log("Hitting a Forward Road");
+                possiblePieces.Remove("deadend-down");
+                possiblePieces.Remove("deadend-left");
+                possiblePieces.Remove("deadend-right");
+                possiblePieces.Remove("straight-horizontal");
+                possiblePieces.Remove("L-down");
+                possiblePieces.Remove("L-left");
+                possiblePieces.Remove("T-down");
+            }    
+        }
+        else
+            Debug.Log("Hitting nothing forward");
+
+        if (possiblePieces.Count <= 0)
+            return "none";
+        else
+            return possiblePieces[Random.Range(0, possiblePieces.Count - 1)];
+    }
 
     private void FillDictionary() //fills the dictionary "pieces" with all 17 possible piece types/rotations
     {
         GameObject temp = Instantiate(roadPiece[0]);
         temp.transform.rotation = Quaternion.Euler(0, 0, 0);
-        pieces.Add("empty", temp);
+        pieces.Add("empty", temp); //empty piece
         Destroy(temp);  
 
         temp = Instantiate(roadPiece[1]);
         temp.transform.rotation = Quaternion.Euler(0, 0, 0);
-        pieces.Add("border", temp);
+        pieces.Add("border", temp); //border piece
         Destroy(temp);
 
         temp = Instantiate(roadPiece[2]);
         temp.transform.rotation = Quaternion.Euler(0, 0, 0);
-        pieces.Add("deadend-down", temp);
+        pieces.Add("deadend-down", temp); //deadend piece with road side facing down
         Destroy(temp);
 
         temp = Instantiate(roadPiece[2]);
         temp.transform.rotation = Quaternion.Euler(0, 90, 0);
-        pieces.Add("deadend-left", temp);
+        pieces.Add("deadend-left", temp); //deadend piece with road side facing left
         Destroy(temp);
 
         temp = Instantiate(roadPiece[2]);
         temp.transform.rotation = Quaternion.Euler(0, 180, 0);
-        pieces.Add("deadend-up", temp);
+        pieces.Add("deadend-up", temp); //deadend piece with road side facing up
         Destroy(temp);
 
         temp = Instantiate(roadPiece[2]);
         temp.transform.rotation = Quaternion.Euler(0, 270, 0);
-        pieces.Add("deadend-right", temp);
+        pieces.Add("deadend-right", temp); //deadend piece with road side facing right
         Destroy(temp);
 
         temp = Instantiate(roadPiece[3]);
         temp.transform.rotation = Quaternion.Euler(0, 0, 0);
-        pieces.Add("straight-vertical", temp);
+        pieces.Add("straight-vertical", temp); //straight piece with road sides going vertical
         Destroy(temp);
 
         temp = Instantiate(roadPiece[3]);
         temp.transform.rotation = Quaternion.Euler(0, 90, 0);
-        pieces.Add("straight-horizontal", temp);
+        pieces.Add("straight-horizontal", temp); //straight piece with road sides going horizontal
         Destroy(temp);
 
         temp = Instantiate(roadPiece[4]);
         temp.transform.rotation = Quaternion.Euler(0, 0, 0);
-        pieces.Add("L-down", temp);
+        pieces.Add("L-down", temp); //L piece with the bottom of the L facing down
         Destroy(temp);
 
         temp = Instantiate(roadPiece[4]);
         temp.transform.rotation = Quaternion.Euler(0, 90, 0);
-        pieces.Add("L-left", temp);
+        pieces.Add("L-left", temp); //L piece with the bottom of the L facing left
         Destroy(temp);
 
         temp = Instantiate(roadPiece[4]);
         temp.transform.rotation = Quaternion.Euler(0, 180, 0);
-        pieces.Add("L-up", temp);
+        pieces.Add("L-up", temp); //L piece with the bottom of the L facing up
         Destroy(temp);
 
         temp = Instantiate(roadPiece[4]);
         temp.transform.rotation = Quaternion.Euler(0, 270, 0);
-        pieces.Add("L-right", temp);
+        pieces.Add("L-right", temp); //L piece with the bottom of the L facing right
         Destroy(temp);
 
         temp = Instantiate(roadPiece[5]);
         temp.transform.rotation = Quaternion.Euler(0, 0, 0);
-        pieces.Add("T-down", temp);
+        pieces.Add("T-down", temp); //T piece with the bottom of the T facing down
         Destroy(temp);
 
         temp = Instantiate(roadPiece[5]);
         temp.transform.rotation = Quaternion.Euler(0, 90, 0);
-        pieces.Add("T-left", temp);
+        pieces.Add("T-left", temp); //T piece with the bottom of the T facing left
         Destroy(temp);
 
         temp = Instantiate(roadPiece[5]);
         temp.transform.rotation = Quaternion.Euler(0, 180, 0);
-        pieces.Add("T-up", temp);
+        pieces.Add("T-up", temp); //T piece with the bottom of the T facing up
         Destroy(temp);
 
         temp = Instantiate(roadPiece[5]);
         temp.transform.rotation = Quaternion.Euler(0, 270, 0);
-        pieces.Add("T-right", temp);
+        pieces.Add("T-right", temp); //T piece with the bottom of the T facing right
         Destroy(temp);
 
         temp = Instantiate(roadPiece[6]);
         temp.transform.rotation = Quaternion.Euler(0, 0, 0);
-        pieces.Add("cross", temp);
+        pieces.Add("cross", temp); //cross piece
         Destroy(temp);
 
     }
